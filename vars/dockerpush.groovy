17d0ab1569311d93b595cf1fdcc9ec023062d590
negiminakshi <124min421@gmail.com>
2025-10-21 18:48:31 +0530

    Add dockerpush function for Docker image deployment

---
+++ vars/dockerpush.groovy
@@ -0,0 +1,6 @@
+def call(String Project, String ImageTag, String dockerhubuser){
+  withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+      sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}"
+  }
+  sh "docker push ${DOCKER_USER}/${Project}:${ImageTag}"
+}
