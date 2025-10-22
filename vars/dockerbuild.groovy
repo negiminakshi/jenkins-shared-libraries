721c31b3144afd40db57abeee7757a4f8d148884
negiminakshi <124min421@gmail.com>
2025-10-22 07:00:05 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,4 +1,4 @@
 // Define function
-def call(String ProjectName, String ImageTag, String Docker_User){
-  sh "docker build -t ${Docker_User}/${ProjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag){
+  sh "docker build -t ${ProjectName}:${ImageTag} ."
 }
